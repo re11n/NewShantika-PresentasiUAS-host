c9c44c0ccfa1b95ce1de879e3aed7db8bcce0e7f
re11n <ryansiahaan@student.telkomuniversity.ac.id>
2023-01-06 20:56:50 +0700

    ubah dikit

--- client/src/components/BusForm.js
+++ client/src/components/BusForm.js
@@ -101,7 +101,7 @@ function BusForm({
           <Col lg={12} xs={24}>
             <Form.Item label="Type" name="type">
               <select name="" id="">
-                <option value="AC">AC</option>
+                <option value="AC" selected="selected">AC</option>
                 <option value="Non-AC">Non-AC</option>
               </select>
             </Form.Item>
