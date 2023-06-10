9629b6383472877486a789c63880d8b37f930a42
re11n <97107417+re11n@users.noreply.github.com>
2023-06-10 23:57:23 +0700

    test

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import {DisableReactDevTools} from "@fvilers/disable-react-devtools";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV === "production") DisableReactDevTools();
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
